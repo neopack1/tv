6f5b269b0caea7c38759a3ffaed7e4a781b46ff5
Piotrek <neopack1@gmail.com>
2017-02-24 08:08:29 +0100

    update

---
+++ _SYNCHRONIZACJA.sh
@@ -0,0 +1,13 @@
+clear;echo;echo;echo AKTUALIZACJA ZDALNEGO REPOZYTORIUM;sleep 1;clear
+#########################################################################################################
+
+
+git add -A
+git commit -a -m 'update'
+git push
+
+
+#########################################################################################################
+sleep 1;echo;echo;echo ZAKOŃCZONO PRZETWARZANIE SKRYPTU;
+sleep 1;clear;echo;echo ZAMYKAM TERMINAL;
+sleep 1; exit
